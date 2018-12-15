f4d65c45ac104f9f20c1817fc2f8defa57d328d2
sayo <@example.com>
2018-12-15 15:38:09 +0900

    first commit

---
+++ demo_app/models.py
@@ -0,0 +1,70 @@
+from django.db import models
+from datetime import date
+# Create your models here.
+# こちらでテーブルの中身を定義します
+class Customers(models.Model):
+    # 選択肢の中身を定義 左：実際にDBに格納される値、右：UIに表示される値
+    education_options = (
+    (1, 'graduate_school'),
+    (2, 'university'),
+    (3, 'high school'),
+    (4, 'other'),
+    )
+
+    marital_options = (
+    (1, 'married'),
+    (2, 'single'),
+    (3, 'others'),
+    )
+
+    gender_options = (
+    (1,'male'),
+    (2,'female'),
+    )
+
+    payment_history = (
+    (-1, 'pay early'),
+    (0, 'pay dully'),
+    (1, '1month_dalay'),
+    (2, '2months_dlay'),
+    )
+
+    # DBのカラム（列名）に相当する部分の定義
+    id = models.AutoField(primary_key=True)
+    last_name = models.CharField(max_length=30)
+    first_name =models.CharField(max_length=30)
+    limit_balance = models.IntegerField(default=100000)
+    sex = models.IntegerField(choices=gender_options, default=1)
+    education = models.IntegerField(choices=education_options, default=1)
+    marriage = models.IntegerField(choices=marital_options, default=1)
+    age = models.IntegerField()
+    pay_0 = models.IntegerField(choices=payment_history, default=0)
+    pay_2 = models.IntegerField(choices=payment_history, default=0)
+    pay_3 = models.IntegerField(choices=payment_history, default=0)
+    pay_4 = models.IntegerField(choices=payment_history, default=0)
+    pay_5 = models.IntegerField(choices=payment_history, default=0)
+    pay_6 = models.IntegerField(choices=payment_history, default=0)
+    bill_amt_1 = models.IntegerField(default=0.0)
+    pay_amt_1 = models.IntegerField(default=5000)
+    pay_amt_2 = models.IntegerField(default=5000)
+    pay_amt_3 = models.IntegerField(default=5000)
+    pay_amt_4 = models.IntegerField(default=5000)
+    pay_amt_5 = models.IntegerField(default=5000)
+    pay_amt_6 = models.IntegerField(default=5000)
+    result = models.IntegerField(blank=True, null=True)
+    proba = models.FloatField(default=0.0)
+    comment = models.CharField(max_length=200, blank=True, null=True)
+    registered_date = models.DateField(default=date.today)
+
+
+    # モデルに関する関数をここで定義
+    def register(self):
+        self.registered_date = date.today()
+        self.save()
+
+    # 管理画面に表示方法を定義：必須項目が入っているかどうかで表示内容を分ける
+    def __str__(self):
+        if self.proba == 0.0:
+            return '%s, %d, %s' % (self.registered_date.strftime('%Y-%m-%d'), self.id, self.last_name+self.first_name)
+        else:
+            return '%s, %d, %s, %d, %s, %s' % (self.registered_date.strftime('%Y-%m-%d'), self.id, self.last_name+self.first_name, self.result, '{}%'.format(round(self.proba*100, 2)), self.comment)
