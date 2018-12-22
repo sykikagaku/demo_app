9ca76cdbbec06be7adc1ef9626083464de03c98f
sayo <@example.com>
2018-12-22 15:20:03 +0900

    add index.html

--- demo_app/views.py
+++ demo_app/views.py
@@ -5,7 +5,9 @@ from sklearn.externals import joblib
 import numpy as np
 
 # モデルの読み込み 関数の前に置いておくこと　毎回読み込むと結構処理量が多い
-loaded_model = joblib.load('demo_app/demo_model.pkl')
+# loaded_model = joblib.load('demo_app/demo_model.pkl')
+loaded_model = joblib.load('/home/sykikagaku/sykikagaku.pythonanywhere.com/demo_app/demo_model.pkl')
+
 
 def index(request):
     return render(request,'demo_app/index.html',{})
